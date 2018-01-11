95e309aefccfefaba42e5a77e9b5b07b03373d5c
siscely <siscely.george@gmail.com>
2018-01-11 02:45:41 +0000

    code testing

---
+++ app/serializers/list_serializer.rb
@@ -0,0 +1,12 @@
+class ListSerializer < ActiveModel::Serializer
+  attributes :id, :created_at, :name
+  
+   def name
+    object.name
+  end
+
+  def created_at
+    object.created_at.strftime('%B %d, %Y')
+  end
+   
+end
