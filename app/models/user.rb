98e053875a5a99443cec49b7e25b16cba3cf9ddd
siscely <siscely.george@gmail.com>
2018-01-12 17:38:35 +0000

    creating from the command line code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
     has_many :lists
     
+  validates :name, presence: true
+ 
+  validates :password, presence: true
+ 
+    
     def serialize
         hash = {}
         attributes.each{ |key, val| hash[key] = val }
